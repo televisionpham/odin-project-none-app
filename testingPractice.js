36b96d4adfb7e67179ed6a3836e1c37914914ca3
Van Pham <televisionpham@gmail.com>
2023-01-11 08:56:11 +0700

    kind of complete test practice

--- testingPractice.js
+++ testingPractice.js
@@ -4,9 +4,47 @@ const functions = (function () {
     return result;
   }
 
+  function reveseString(str) {
+    let result = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+      result += str[i];
+    }
+
+    return result;
+  }
+
   return {
     capitalize,
+    reveseString,
   };
 })();
 
-export default functions;
+const calculator = (function () {
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function divide(a, b) {
+    if (b !== 0) {
+      return a / b;
+    }
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  return {
+    add,
+    subtract,
+    divide,
+    multiply,
+  };
+})();
+
+export { functions, calculator };
--- testingPractice.test.js
+++ testingPractice.test.js
@@ -1,5 +1,25 @@
-import functions from "./testingPractice";
+import { functions, calculator } from "./testingPractice";
 
-test('capitalize a string', () => {
-    expect(functions.capitalize('abc')).toBe('Abc')
-})
+test("capitalize a string", () => {
+  expect(functions.capitalize("abc")).toBe("Abc");
+});
+
+test("reverse string", () => {
+  expect(functions.reveseString("abc")).toBe("cba");
+});
+
+test("add a to b", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("subtract a to b", () => {
+  expect(calculator.subtract(2, 1)).toBe(1);
+});
+
+test("divide a to b", () => {
+  expect(calculator.divide(6, 2)).toBe(3);
+});
+
+test("multiply a to b", () => {
+  expect(calculator.multiply(1, 3)).toBe(3);
+});
